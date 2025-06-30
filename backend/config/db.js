b94f6cee867c102a2eeb1a428b034a33ee253634
hoquocnam92 <hoquocnam92@gmail.com>
2025-06-30 10:52:28 +0700

    add mysql

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,12 @@ export const querySQL = async (query) => {
     try {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST || 'db',
+            port: process.env.DB_PORT || 3306,
             user: process.env.DB_USER || 'root',
             password: process.env.DB_PASSWORD || '123456',
             database: process.env.DB_NAME || 'bai3',
         });
+
 
         const [rows] = await connection.execute(query);
         return rows;
